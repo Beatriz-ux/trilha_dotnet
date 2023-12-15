4ba855122a636cecea09f949666da5935c90847f
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-15 09:12:16 -0300

    feat: root to see Beatriz informations

--- DOTNET-P002/Program.cs
+++ DOTNET-P002/Program.cs
@@ -1,6 +1,8 @@
+using DOTNET_P002;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/beatriz", () => Beatriz.View());
 
 app.Run();
