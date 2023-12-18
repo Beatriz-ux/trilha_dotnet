4fa9f9a80d4ebe2754deed2becb3b354589b5272
BrendomGoncalves <brendom.vi@gmail.com>
2023-12-18 20:01:53 -0300

    Criando entidades no domain

---
+++ CleanArchitecture/Core/CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class User : BaseEntity
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
