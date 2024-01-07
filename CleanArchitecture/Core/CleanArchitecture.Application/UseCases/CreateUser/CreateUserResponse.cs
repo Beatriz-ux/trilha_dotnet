a814fa4e9298361de20eb8020659f658d568ce9d
BrendomGoncalves <brendom.vi@gmail.com>
2024-01-07 14:19:10 -0300

    Criando casos de uso para User

---
+++ CleanArchitecture/Core/CleanArchitecture.Application/UseCases/CreateUser/CreateUserResponse.cs
@@ -0,0 +1,7 @@
+namespace CleanArchitecture.Application.UseCases.CreateUser;
+
+public sealed record CreateUserResponse{
+    public Guid Id {get; set;}
+    public string? Email {get; set;}
+    public string? Name {get; set;}
+}
---
+++ CleanArchitecture/Core/CleanArchitecture.Application/UseCases/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,10 @@
+using FluentValidation;
+
+namespace CleanArchitecture.Application.UseCases.CreateUser;
+
+public sealed class CreateUserValidator : AbstractValidator<CreateUserRequest>{
+    public CreateUserValidator(){
+        RuleFor(x => x.Email).NotEmpty().MaximumLength(50).EmailAddress();
+        RuleFor(x => x.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
+    }
+}
