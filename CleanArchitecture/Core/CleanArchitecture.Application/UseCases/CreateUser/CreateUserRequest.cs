04c74cb5f10c62338214934835590b20141b8083
albertolunia <albertolunia@gmail.com>
2024-01-07 21:59:31 -0300

    Finalizado ID 4

---
+++ CleanArchitecture/Core/CleanArchitecture.Application/UseCases/CreateUser/CreateUserRequest.cs
@@ -0,0 +1,6 @@
+using MediatR;
+
+namespace CleanArchitecture.Application.UseCases.CreateUser
+{
+    public sealed record CreateUserRequest(string Email, string Name) : IRequest<CreateUserResponse>;
+}
