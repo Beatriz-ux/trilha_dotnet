3c6294439db21e4d012c9d4ddcd764e953795eec
Lucas Silva <lucas_avlis@outlook.com>
2024-01-13 11:29:22 -0300

    Subindo tarefa Clean Architecture II

---
+++ CleanArchitecture/Infrastructure/CleanArchitecture.Persitence/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persitence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Commit(CancellationToken cancellationToken)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ CleanArchitecture/Infrastructure/CleanArchitecture.Persitence/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence.Repositories;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context)
+    { }
+
+    public async Task<User> GetByEmail(string email, CancellationToken cancellationToken)
+    {
+        return await Context.Users.FirstOrDefaultAsync(x => x.Email == email, cancellationToken);
+    }
+}
