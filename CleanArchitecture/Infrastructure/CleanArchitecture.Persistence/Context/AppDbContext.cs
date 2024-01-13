0fdb323500da27732fcf7796aa7f56bdd79b6cfc
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-13 20:51:05 -0300

    feat: implementação da infraestrutura

---
+++ CleanArchitecture/Infrastructure/CleanArchitecture.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence.Context;
+
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ CleanArchitecture/Infrastructure/CleanArchitecture.Persistence/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CleanArchitecture.Persistence.Context;
+
+
+
+namespace CleanArchitecture.Persistence;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration)
+
+    {
+        var connectionString = configuration.GetConnectionString("Sqlite");
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionString));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+    }
+
+}
