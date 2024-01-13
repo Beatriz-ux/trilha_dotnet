4df60c977acfc0b22bc1d60da7875d9afd9146f1
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-13 18:48:40 -0300

    fix: herdando BaseEntity

--- CleanArchitecture/Core/Domain/Entities/User.cs
+++ CleanArchitecture/Core/Domain/Entities/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace CleanArchitecture.Domain.Entities;
 
-public sealed class User 
+public sealed class User : BaseEntity
 {
     public  string? Email { get; set; }
     public  string? Name { get; set; }
