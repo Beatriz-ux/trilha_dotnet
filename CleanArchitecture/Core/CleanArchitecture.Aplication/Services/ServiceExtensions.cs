16126f3bfe7e02b0f601bb2134868cd817741995
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-13 22:49:48 -0300

    feat: ServiceExtensions

---
+++ CleanArchitecture/Core/CleanArchitecture.Aplication/Services/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace CleanArchitecture.Aplication;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplicationApp(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+}
