8255aba07a037927d0b065c0181748922b14d699
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-13 22:38:06 -0300

    feat: implementando o CreateUser na camada aplication

---
+++ CleanArchitecture/Core/CleanArchitecture.Aplication/UseCases/CreateUser/CreateUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Aplication.UseCases.CreateUser;
+
+public sealed record CreateUserResponse
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+}
