081dbb8b28a5ed2082d4c23e80a8174686494e0f
albertolunia <albertolunia@gmail.com>
2024-01-17 22:01:45 -0300

    Fazendo o basico do appdbcontext

---
+++ ResTIConnect.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ResTIConnect.Infra.Data.Context{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+
+}
