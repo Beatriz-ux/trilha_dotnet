7a365cc5154639b47144d2d56c799a44184d8c9f
albertolunia <albertolunia@gmail.com>
2024-01-17 23:07:52 -0300

    Migrations criadas

--- ResTIConnect.Infra.Data/Context/AppDbContext.cs
+++ ResTIConnect.Infra.Data/Context/AppDbContext.cs
@@ -3,9 +3,19 @@ using Microsoft.EntityFrameworkCore;
 namespace ResTIConnect.Infra.Data.Context{
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            base.OnConfiguring(optionsBuilder);
+            var connectionString = "server=localhost;user=resticonnect;password=Beto@9999;database=resticonnect;";
+            var serverVersion = ServerVersion.AutoDetect(connectionString);
             
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
