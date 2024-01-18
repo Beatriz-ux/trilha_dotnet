7e796f9dd40e83409d79b5b1a5bae7ce48b77f31
albertolunia <albertolunia@gmail.com>
2024-01-17 21:08:08 -0300

    Script de criação do usuario

---
+++ files/database/01_create_usuario.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuario (
+    idUsuario INT PRIMARY KEY NOT NULL,
+    nomeUsuario VARCHAR(255) NOT NULL,
+    emailUsuario VARCHAR(255) NOT NULL,
+    senhaUsuario VARCHAR(255) NOT NULL
+);
