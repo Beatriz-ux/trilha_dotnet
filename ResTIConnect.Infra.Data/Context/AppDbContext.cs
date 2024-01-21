5adb9eb30642d8bc647da34369733dc1621e163e
Lucas Silva <lucas_avlis@outlook.com>
2024-01-21 01:11:30 -0300

    Criando models Perfis e Enderecos e suas migrations

--- ResTIConnect.Infra.Data/Context/AppDbContext.cs
+++ ResTIConnect.Infra.Data/Context/AppDbContext.cs
@@ -1,15 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using ResTIConnect.Infra.Data.Models;
 
-namespace ResTIConnect.Infra.Data.Context{
+namespace ResTIConnect.Infra.Data.Context
+{
     public class AppDbContext : DbContext
     {
-
+        public DbSet<Perfis> Perfis { get; set; }
+        public DbSet<Enderecos> Enderecos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            var connectionString = "server=localhost;user=resticonnect;password=Beto@9999;database=resticonnect;";
+            var connectionString = "server=localhost;user=root;password=123456;database=resticonnect;";
             var serverVersion = ServerVersion.AutoDetect(connectionString);
-            
+
             optionsBuilder.UseMySql(connectionString, serverVersion);
         }
 
---
+++ ResTIConnect.Infra.Data/Models/Perfis.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResTIConnect.Infra.Data.Models
+{
+    public class Perfis
+    {
+         public int Id { get; set; }
+         public string Descricao {get; set;}
+         public string Permissoes {get; set;}
+         
+    }
+}
