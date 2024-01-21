21980df4b20ad56531ae7becf078e1ded1a52d0e
albertolunia <albertolunia@gmail.com>
2024-01-21 19:38:24 -0300

    Criando context

---
+++ Context/AppDbContext/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace Context;
+
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Entities;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Conta> Contas { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var connectionString = "server=localhost;user=projetopessoal;password=Beto@9999;database=projetopessoal;";
+            var serverVersion = ServerVersion.AutoDetect(connectionString);
+            
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+}
