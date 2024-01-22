05069af713dddf34775e67509ecc43958053004f
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-01-22 00:01:02 -0300

    Criação da entidade Transacao

---
+++ Entities/Transacao.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities;
+
+public class Transacao
+{
+    public int IdTransacao { get; set; }
+    public float ValorTransacao { get; set; }
+    public DateTime DataTransacao { get; set; }
+    public string? TipoTransacao { get; set; }
+    public int IdConta { get; set; }
+    public int IdCategoria { get; set; }
+
+}
