83029452f6894ecc92241125d400cc5a71a079b1
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-01-21 23:55:53 -0300

    Criação da entidade Custo Variavel

---
+++ Entities/CustoVariavel.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities;
+
+public class CustoVariavel
+{
+    public int IdCustoVariavel { get; set; }
+    public float ValorVariavel { get; set; }
+    public DateTime DataPlanejadaVariavel  { get; set; }
+    public int IdConta { get; set; }
+    public int IdCategoria { get; set; }
+
+}
