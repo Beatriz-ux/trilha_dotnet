3e7dc3b176ffbfe889ef17182c0814e579254ade
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-22 16:40:22 -0300

    fix: passando o BaseEntity para a pasta de entites

---
+++ ResTIConnect.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ResTIConnect.Domain.Entities; //troquei para a pasta Entities , antes estava na pasta Common. No Cepedi eles usam nessa pasta
+
+public abstract class BaseEntity
+{
+   // public Guid Id { get; set; } // como cada tabela tem id proprio, não precisa mais do id aqui
+    // public DateTimeOffset DateCreated { get; set; }
+    // public DateTimeOffset? DateUpdated { get; set; }
+    // public DateTimeOffset? DateDeleted { get; set; }
+}
