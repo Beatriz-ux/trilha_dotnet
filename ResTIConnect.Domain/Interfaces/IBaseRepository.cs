03d72c5a65c8caf9533e10a45c1394afdea0dddc
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-22 16:41:20 -0300

    fix: removendo a busca por id, pois cada entidde vai fazer o seu

--- ResTIConnect.Domain/Interfaces/IBaseRepository.cs
+++ ResTIConnect.Domain/Interfaces/IBaseRepository.cs
@@ -1,4 +1,4 @@
-using ResTIConnect.Domain.Common;
+using ResTIConnect.Domain.Entities;
 
 namespace ResTIConnect.Domain.Interfaces;
 
@@ -7,6 +7,5 @@ public interface IBaseRepository<T> where T : BaseEntity
     void Create(T entity);
     void Update(T entity);
     void Delete(T entity);
-    Task<T> Get(Guid id, CancellationToken cancellationToken);
     Task<List<T>> GetAll(CancellationToken cancellationToken);
 }
