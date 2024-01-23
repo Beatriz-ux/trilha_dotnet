2ea666abc4e4082c251fa25b1f25f2c2d7ba50fa
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-23 14:12:07 -0300

    feat: tabela com o relacionamento

--- Entities/Entities/ObjetivoInvestimento.cs
+++ Entities/Entities/ObjetivoInvestimento.cs
@@ -2,6 +2,7 @@
 
 public class ObjetivoInvestimento
 {
+    public int Id { get; set; }
     public int IdObjetivo { get; set; }
     public int IdInvestimento { get; set; }
     public Objetivo Objetivo { get; set; }
