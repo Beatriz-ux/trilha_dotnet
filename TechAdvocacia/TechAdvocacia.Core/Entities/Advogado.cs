257e6579c64b824d6956bb589d198c0ae90c895d
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-30 16:06:56 -0300

    feat:  lista de casos juridicos

--- TechAdvocacia/TechAdvocacia.Core/Entities/Advogado.cs
+++ TechAdvocacia/TechAdvocacia.Core/Entities/Advogado.cs
@@ -1,8 +1,11 @@
-﻿namespace TechAdvocacia.Core.Entities;
+﻿
+namespace TechAdvocacia.Core.Entities;
 
 public class Advogado : Pessoa
 {
     public int AdvogadoId { get; set; }
     public string CNA { get; set; }
+
+    public List<CasoJuridico> CasosJuridicos { get; set; } = new List<CasoJuridico>();
 
 }
