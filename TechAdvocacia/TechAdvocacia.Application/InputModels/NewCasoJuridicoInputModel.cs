4cf4ebc0ed24ae8636ee75c4cdf89fd4ff9cf741
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-01-30 16:59:31 -0300

    Implemtação de ViewModel, InputModel e Services

---
+++ TechAdvocacia/TechAdvocacia.Application/InputModels/NewCasoJuridicoInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TechAdvocacia.Application.InputModel;
+
+public class NewCasoJuridicoInputModel
+{
+    public DateTime Abertura { get; set; }
+    public int ClienteId {get; set;}
+    public int AdvogadoId {get; set;}
+    public int DocumentoId {get; set;}
+}
