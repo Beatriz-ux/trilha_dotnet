31d87a39496e62b6ab1710e228ae56d7cfb6bc03
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-30 16:53:49 -0300

    feat: service

--- TechAdvocacia/TechAdvocacia.Application/Services/ClienteService.cs
+++ TechAdvocacia/TechAdvocacia.Application/Services/ClienteService.cs
@@ -1,13 +1,27 @@
 ﻿using TechAdvocacia.Application.InputModels;
-
+using TechAdvocacia.Application.Services.Interfaces;
+using TechAdvocacia.Core.Entities;
+using TechAdvocacia.Infrastructure.Persistence;
 namespace TechAdvocacia.Application.Services;
 
 public class ClienteService : IClienteService
 {
     private readonly TechAdvocaciaDbContext _dbContext;
+
+    public ClienteService(TechAdvocaciaDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public int Create(NewClienteInputModel cliente)
     {
-        throw new NotImplementedException();
+        var _cliente = new Cliente{
+            
+            Nome = cliente.Nome
+        };
+        _dbContext.Clientes.Add(_cliente);
+        _dbContext.SaveChanges();
+
+        return _cliente.ClienteId;
     }
 
     public void Delete(int id)
