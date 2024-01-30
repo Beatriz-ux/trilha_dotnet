d9de356119a4e275db0f85d0d812fdcf29dbfc02
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-30 16:36:36 -0300

    feat: Interfaces com o CRud de advogado e cliente

---
+++ TechAdvocacia/TechAdvocacia.Application/Services/Interfaces/IAdvogado.cs
@@ -0,0 +1,13 @@
+﻿using TechAdvocacia.Application.InputModels;
+
+namespace TechAdvocacia.Application.Services.Interfaces;
+
+public interface IAdvogado
+{
+    public List<AdvogadoViewModel> GetAll();
+    public AdvogadoViewModel? GetById(int id);
+    public int Create(NewAdvogadoInputModel advogado);
+    public void Update(int Id,NewAdvogadoInputModel advogado);
+    public void Delete(int id);
+
+}
