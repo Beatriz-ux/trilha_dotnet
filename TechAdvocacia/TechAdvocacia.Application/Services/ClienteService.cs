3e6d2032a51cf1cf2caa38d9eb95fdb0eef41074
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-30 16:38:56 -0300

    feat: dependencias

---
+++ TechAdvocacia/TechAdvocacia.Application/Services/ClienteService.cs
@@ -0,0 +1,32 @@
+﻿using TechAdvocacia.Application.InputModels;
+
+namespace TechAdvocacia.Application.Services;
+
+public class ClienteService : IClienteService
+{
+    private readonly TechAdvocaciaDbContext _dbContext;
+    public int Create(NewClienteInputModel cliente)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<ClienteViewModel> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ClienteViewModel? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(int Id, NewClienteInputModel cliente)
+    {
+        throw new NotImplementedException();
+    }
+}
