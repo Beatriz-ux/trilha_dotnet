00a2c203292dca209b7108ce3b20656c722d6f37
BrendomGoncalves <brendom.vi@gmail.com>
2024-02-10 00:35:52 -0300

    Endpoints do crud de sistemas

---
+++ ResTIConnect.API/Controllers/SistemaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResTIConnect.Application.InputModels;
+using ResTIConnect.Application.Services.Interfaces;
+using ResTIConnect.Application.ViewModels;
+
+namespace ResTIConnect.API.Controllers;
+
+public class SistemaController : ControllerBase
+{
+    private readonly ISistemaService _sistemaService;
+    public List<SistemaViewModel> Sistemas => _sistemaService.GetAll();
+
+    public SistemaController(ISistemaService sistemaService)
+    {
+        _sistemaService = sistemaService;
+    }
+
+    [HttpGet("sistemas")]
+    public IActionResult Get()
+    {
+        return Ok(Sistemas);
+    }
+
+    [HttpGet("sistemas/{id}")]
+    public IActionResult Get(int id)
+    {
+        try
+        {
+            var sistema = _sistemaService.GetById(id);
+            return Ok(sistema);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost("sistemas")]
+    public IActionResult Post([FromBody] NewSistemaInputModel sistema)
+    {
+        _sistemaService.Create(sistema);
+        return CreatedAtAction(nameof(Get), sistema);
+    }
+
+    [HttpPut("sistemas/{id}")]
+    public IActionResult Put(int id, [FromBody] NewSistemaInputModel sistema)
+    {
+        if(_sistemaService.GetById(id) == null) return NoContent();
+        _sistemaService.Update(id, sistema);
+        return Ok(_sistemaService.GetById(id));
+    }
+
+    [HttpDelete("sistemas/{id}")]
+    public IActionResult Delete(int id)
+    {
+        if(_sistemaService.GetById(id) == null) return NoContent();
+        _sistemaService.Delete(id);
+        return Ok();
+    }
+}
---
+++ ResTIConnect.Application/Services/SistemaService.cs
@@ -0,0 +1,90 @@
+﻿using ResTIConnect.Application.InputModels;
+using ResTIConnect.Application.Services.Interfaces;
+using ResTIConnect.Application.ViewModels;
+using ResTIConnect.Infra.Data.Context;
+using ResTIConnect.Infra.Data.Models;
+
+namespace ResTIConnect.Application.Services;
+
+public class SistemaService : ISistemaService
+{
+    private readonly AppDbContext _context;
+    public SistemaService(AppDbContext context)
+    {
+        _context = context;
+    }
+    public int Create(NewSistemaInputModel sistema)
+    {
+        var _sistema = new Sistemas
+        {
+            Descricao = sistema.Descricao,
+            Tipo = sistema.Tipo,
+            EnderecoEntrada = sistema.EnderecoEntrada,
+            EnderecoSaida = sistema.EnderecoSaida,
+            Protocolo = sistema.Protocolo,
+            DataHoraInicioIntegracao = sistema.DataHoraInicioIntegracao,
+            Status = sistema.Status
+        };
+        _context.Sistemas.Add(_sistema);
+        _context.SaveChanges();
+        return _sistema.SistemaId;
+    }
+    public void Delete(int id)
+    {
+        _context.Sistemas.Remove(GetByDbId(id));
+        _context.SaveChanges();
+    }
+    private Sistemas GetByDbId(int id)
+    {
+        var _sistema = _context.Sistemas.Find(id);
+        if (_sistema == null)
+        {
+            return null;
+        }
+        return _sistema;
+    }
+    public List<SistemaViewModel> GetAll()
+    {
+        var _sistemas = _context.Sistemas.Select(s => new SistemaViewModel
+        {
+            SistemaId = s.SistemaId,
+            Descricao = s.Descricao,
+            Tipo = s.Tipo,
+            EnderecoEntrada = s.EnderecoEntrada,
+            EnderecoSaida = s.EnderecoSaida,
+            Protocolo = s.Protocolo,
+            DataHoraInicioIntegracao = s.DataHoraInicioIntegracao,
+            Status = s.Status
+        }).ToList();
+        return _sistemas;
+    }
+    public SistemaViewModel? GetById(int id)
+    {
+        var _sistema = GetByDbId(id);
+        var SistemaViewModel = new SistemaViewModel
+        {
+            SistemaId = _sistema.SistemaId,
+            Descricao = _sistema.Descricao,
+            Tipo = _sistema.Tipo,
+            EnderecoEntrada = _sistema.EnderecoEntrada,
+            EnderecoSaida = _sistema.EnderecoSaida,
+            Protocolo = _sistema.Protocolo,
+            DataHoraInicioIntegracao = _sistema.DataHoraInicioIntegracao,
+            Status = _sistema.Status
+        };
+        return SistemaViewModel;
+    }
+    public void Update(int id, NewSistemaInputModel sistema)
+    {
+        var _sistema = GetByDbId(id);
+        _sistema.Descricao = sistema.Descricao;
+        _sistema.Tipo = sistema.Tipo;
+        _sistema.EnderecoEntrada = sistema.EnderecoEntrada;
+        _sistema.EnderecoSaida = sistema.EnderecoSaida;
+        _sistema.Protocolo = sistema.Protocolo;
+        _sistema.DataHoraInicioIntegracao = sistema.DataHoraInicioIntegracao;
+        _sistema.Status = sistema.Status;
+        _context.Sistemas.Update(_sistema);
+        _context.SaveChanges();
+    }
+}
