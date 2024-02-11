180f146b47f6e85117277aade87208df21f6b587
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-02-11 17:20:28 -0300

    feat: endpoints de objetivo

--- Financa.Core/Entities/Objetivo.cs
+++ Financa.Core/Entities/Objetivo.cs
@@ -5,7 +5,10 @@ public class Objetivo
     public int Id { get; set; }
     public string Nome { get; set; }
 
-    public ICollection<Investimento> Investimentos { get; }
-
+    public ICollection<Investimento> Investimentos { get; set; }
+    public Objetivo()
+    {
+        Investimentos = new List<Investimento>();
+    }
 
 }
