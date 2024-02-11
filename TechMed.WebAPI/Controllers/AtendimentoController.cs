e8771589c0c27979198b7b9622dd3fc9c03afc22
BrendomGoncalves <brendom.vi@gmail.com>
2024-02-11 17:50:27 -0300

    Implementação dos controllers e estrutura do program

---
+++ TechMed.WebAPI/Controllers/AtendimentoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechMed.Application.Services.Interfaces;
+using TechMed.Application.ViewModels;
+using TechMed.Application.InputModels;
+
+namespace TechMed.WebAPI.Controllers;
+
+[ApiController]
+[Route("atendimento")]
+public class AtendimentoController : ControllerBase
+{
+    private readonly IAtendimentoService _atendimentoService;
+    public List<AtendimentoViewModel> Atendimentos => _atendimentoService.GetAll();
+    public AtendimentoController(IAtendimentoService service) => _atendimentoService = service;
+
+    [HttpGet("atendimentos")]
+    public IActionResult Get()
+    {
+        return Ok(Atendimentos);
+    }
+
+    [HttpGet("atendimento/{id}")]
+    public IActionResult GetById(int id)
+    {
+        try{
+            var atendimento = _atendimentoService.GetById(id);
+            return Ok(atendimento);
+        } catch (Exception e){
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost("atendimento")]
+    public IActionResult Post([FromBody] AtendimentoInputModel atendimento)
+    {
+        _atendimentoService.Create(atendimento);
+        return CreatedAtAction(nameof(Get), atendimento);
+    }
+
+    [HttpPut("atendimento/{id}")]
+    public IActionResult Put(int id, [FromBody] AtendimentoInputModel atendimento)
+    {
+        if(_atendimentoService.GetById(id) == null) return NoContent();
+        _atendimentoService.Update(id, atendimento);
+        return Ok(_atendimentoService.GetById(id));
+    }
+
+    [HttpDelete("atendimento/{id}")]
+    public IActionResult Delete(int id)
+    {
+        if(_atendimentoService.GetById(id) == null) return NoContent();
+        _atendimentoService.Delete(id);
+        return Ok();
+    }
+}
---
+++ TechMed.WebAPI/Controllers/ExameController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechMed.Application.Services.Interfaces;
+using TechMed.Application.ViewModels;
+using TechMed.Application.InputModels;
+
+namespace TechMed.WebAPI.Controllers;
+
+[ApiController]
+[Route("exame")]
+public class ExameController : ControllerBase
+{
+    private readonly IExameService _exameService;
+    public List<ExameViewModel> Exames => _exameService.GerAll();
+    public ExameController(IExameService service) => _exameService = service;
+
+    [HttpGet("exames")]
+    public IActionResult Get()
+    {
+        return Ok(Exames);
+    }
+
+    [HttpGet("exame/{id}")]
+    public IActionResult GetById(int id)
+    {
+        try{
+            var exame = _exameService.GetById(id);
+            return Ok(exame);
+        } catch (Exception e){
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost("exame")]
+    public IActionResult Post([FromBody] ExameInputModel exame)
+    {
+        _exameService.Create(exame);
+        return CreatedAtAction(nameof(Get), exame);
+    }
+
+    [HttpPut("exame/{id}")]
+    public IActionResult Put(int id, [FromBody] ExameInputModel exame)
+    {
+        if(_exameService.GetById(id) == null) return NoContent();
+        _exameService.Update(id, exame);
+        return Ok(_exameService.GetById(id));
+    }
+
+    [HttpDelete("exame/{id}")]
+    public IActionResult Delete(int id)
+    {
+        if(_exameService.GetById(id) == null) return NoContent();
+        _exameService.Delete(id);
+        return Ok();
+    }
+}
