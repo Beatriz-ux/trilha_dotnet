1e30ee54869aa13c47574f6e82c303de9308db31
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-02-13 11:31:41 -0300

    Add UsuarioViewModel class

---
+++ ResTIConnect.Application/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ResTIConnect.Application.ViewModels;
+
+public class UsuarioViewModel
+{
+    public int UsuarioId { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public EnderecoViewModel Endereco { get; set; }
+
+}
