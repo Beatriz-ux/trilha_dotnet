9af1a34032cc8bb67dc6d7ff1ed47617118924a0
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-02-13 11:30:05 -0300

    Add IUsuarioService interface

---
+++ ResTIConnect.Application/Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using ResTIConnect.Application.InputModels;
+using ResTIConnect.Application.ViewModels;
+
+namespace ResTIConnect.Application.Services.Interfaces;
+
+public interface IUsuarioService
+{
+    public List<UsuarioViewModel> GetAll();
+    public UsuarioViewModel? GetById(int id);
+    public int Create(NewUsuarioInputModel usuario);
+    public void Update(int id, NewUsuarioInputModel usuario);
+    public void Delete(int id);
+
+}
