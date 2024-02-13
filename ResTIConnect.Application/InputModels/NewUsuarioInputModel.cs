c478ee8dda287ba2205a0680e90137b65c452f6d
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-02-13 11:32:08 -0300

    Add NewUsuarioInputModel class

---
+++ ResTIConnect.Application/InputModels/NewUsuarioInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ResTIConnect.Application.InputModels;
+
+public class NewUsuarioInputModel
+{
+    public int UsuarioId { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public string Telefone { get; set; }
+    public int EnderecoId { get; set; }
+
+}
