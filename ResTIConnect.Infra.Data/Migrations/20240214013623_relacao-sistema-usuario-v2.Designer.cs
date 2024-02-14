57565b660fb51fcd54add5c9a5cd91b420674ea0
BrendomGoncalves <brendom.vi@gmail.com>
2024-02-13 22:37:40 -0300

    Criando migration relacao sistema usuario

--- ResTIConnect.Infra.Data/Migrations/20240214013623_relacao-sistema-usuario-v2.Designer.cs
+++ ResTIConnect.Infra.Data/Migrations/20240214013623_relacao-sistema-usuario-v2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResTIConnect.Infra.Data.Context;
 
@@ -10,9 +11,11 @@ using ResTIConnect.Infra.Data.Context;
 namespace ResTIConnect.Infra.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240214013623_relacao-sistema-usuario-v2")]
+    partial class relacaosistemausuariov2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
