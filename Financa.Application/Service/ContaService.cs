1384f3d4d7eef61fe8f27e3693d3484be31e68d8
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-02-21 08:47:43 -0300

    Criacao da interface IContaService

---
+++ Financa.Application/Service/ContaService.cs
@@ -0,0 +1,13 @@
+﻿using Financa.Application.InputModels;
+using Financa.Application.Services.Interfaces;
+using Financa.Application.ViewModels;
+using Financa.Core.Entities;
+using Financa.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financa.Application.Services;
+
+public class ContaService : IContaService
+{
+    
+}
