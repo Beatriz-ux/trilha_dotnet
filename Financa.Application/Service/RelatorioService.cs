be8a4809506b12c4e5957f5a686e591312b1ac7b
albertolunia <albertolunia@gmail.com>
2024-02-22 10:13:05 -0300

    Remove commented out code in RelatorioService.cs

--- Financa.Application/Service/RelatorioService.cs
+++ Financa.Application/Service/RelatorioService.cs
@@ -19,15 +19,10 @@ namespace Financa.Application.Services
             _custoFixoService = custoFixoService;
         }
 
-        // Transições por categoria (Não tem transição ainda)
-        // Contas com custo fixo maior que um valor x (Não tem custo fixo ainda)
-        // Contas com custo variavel maior que um valor x (Não tem custo fixo ainda)
-        // Dias que tiveram mais de X transações (Não tem transação ainda)
-
-        public ICollection<TransacoesViewModel> GetTransicoesByCategoria(int categoriaId)
-        {
-            throw new NotImplementedException();
-        }
+        // public ICollection<TransacoesViewModel> GetTransicoesByCategoria(int categoriaId)
+        // {
+        //     throw new NotImplementedException();
+        // }
         public ICollection<ContaViewModel> GetContasByCustoFixoAboveLimit(double limit)
         {
             throw new NotImplementedException();
