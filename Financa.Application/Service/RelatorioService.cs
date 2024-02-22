5181986559af37a3610dc66071f110bc9da1b3e8
albertolunia <albertolunia@gmail.com>
2024-02-22 10:08:48 -0300

    RelatorioService

---
+++ Financa.Application/Service/RelatorioService.cs
@@ -0,0 +1,27 @@
+using Financa.Application.Services.Interfaces;
+using Financa.Application.ViewModels;
+using Financa.Infrastructure;
+
+namespace Financa.Application.Services
+{
+    public class RelatorioService
+    {
+        private readonly AppDbContext _context;
+        private readonly ICategoriaService _categoriaService;
+        private readonly IContaService _contaService;
+        private readonly ICustoFixoService _custoFixoService;
+
+        public RelatorioService(AppDbContext context, ICategoriaService categoriaService, IContaService contaService, ICustoFixoService custoFixoService)
+        {
+            _context = context;
+            _categoriaService = categoriaService;
+            _contaService = contaService;
+            _custoFixoService = custoFixoService;
+        }
+
+        public ICollection<ContaViewModel> GetContasByCustoFixoAboveLimit(double limit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
