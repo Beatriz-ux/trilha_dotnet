9a98aa3871dde84ddd92305c6b3bd64074d08e94
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-02-29 13:44:10 -0300

    Encrypt user passwords using SHA256 algorithm

---
+++ ResTIConnect.API/Utils/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ResTIConnect.API.Utils;
+public abstract class Utils
+{
+
+    // Método para criptografar a senha
+    public static string EncryptPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            // Convertendo a senha em bytes
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+
+            // Calculando o hash SHA256
+            byte[] hash = sha256.ComputeHash(bytes);
+
+            // Convertendo o hash em uma string Base64
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
