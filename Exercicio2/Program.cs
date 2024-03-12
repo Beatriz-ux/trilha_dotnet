392ff3b2e5f52f9f48c4ac4bdd98b59418ed8a55
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-03-12 15:38:22 -0300

    Fix incorrect minutes value in Data instantiation

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -3,7 +3,7 @@
  int FORMATO_12H = 12;
 int FORMATO_24H = 24;
 
-Data data = new Data(10, 03, 2000, 10, 30, 10);
+Data data = new Data(10, 03, 2000, 10, 70, 10);
 
 data.Imprimir(Data.FORMATO_12H);
 data.Imprimir(Data.FORMATO_24H);
